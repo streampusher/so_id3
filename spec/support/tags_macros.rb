c906f96c9b583714129d5ee556691ad0038181a3
Tony Miller <mcfiredrill@gmail.com>
2014-03-19 22:19:12 +0900

    write the file to s3 again after resetting its tags in spec

--- spec/support/tags_macros.rb
+++ spec/support/tags_macros.rb
@@ -3,6 +3,16 @@ module TagsMacros
     tags = {}
     tags[:artist] = 'dj nameko'
     tags[:title] = 'a cool song'
-    Rupeepeethree::Tagger.tag("spec/support/test.mp3",tags)
+    mp3 = "spec/support/test.mp3"
+    Rupeepeethree::Tagger.tag(mp3, tags)
+
+    s3_credentials = { bucket: ENV['S3_BUCKET'],
+                      access_key_id: ENV['S3_KEY'],
+                      secret_access_key: ENV['S3_SECRET'] }
+    @s3 = AWS::S3.new(access_key_id: s3_credentials[:access_key_id],
+                      secret_access_key: s3_credentials[:secret_access_key])
+    @bucket = @s3.buckets.create(s3_credentials[:bucket])
+    key = File.basename(mp3)
+    @bucket.objects[key].write(file: mp3, acl: :public_read)
   end
 end
