e324cee13361a0586c2ec8844950188547c80d09
Tony Miller <mcfiredrill@gmail.com>
2015-11-20 15:11:22 +0900

    use a specific bucket for testing locally and on travis
    This might make it harder for people to contribute. It might be better
    to stub all aws requests the way paperclip does in their test suite.

--- spec/support/tags_macros.rb
+++ spec/support/tags_macros.rb
@@ -12,10 +12,12 @@ module TagsMacros
     s3_credentials = { bucket: ENV['S3_BUCKET'],
                       access_key_id: ENV['S3_KEY'],
                       secret_access_key: ENV['S3_SECRET'] }
-    @s3 = AWS::S3.new(access_key_id: s3_credentials[:access_key_id],
-                      secret_access_key: s3_credentials[:secret_access_key])
-    @bucket = @s3.buckets.create(s3_credentials[:bucket])
-    key = File.basename(mp3)
-    @bucket.objects[key].write(file: mp3, acl: :public_read)
+    VCR.use_cassette "reset_s3_object" do
+      @s3 = AWS::S3.new(access_key_id: s3_credentials[:access_key_id],
+                        secret_access_key: s3_credentials[:secret_access_key])
+      @bucket = @s3.buckets.create(s3_credentials[:bucket])
+      key = File.basename(mp3)
+      @bucket.objects[key].write(file: mp3, acl: :public_read)
+    end
   end
 end
